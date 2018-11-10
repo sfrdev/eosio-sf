61f6bdbad819af76c647793fe60572319f00b159
Mohammed Roshan <roshan@throughbit.com>
2018-11-10 12:54:39 -0800

    First

---
+++ eosio_docker/contracts/hacksf/hacksf.cpp
@@ -0,0 +1,143 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/asset.hpp>
+#include <string>
+using namespace eosio;
+
+using std::string;
+class hacksf : public eosio::contract {
+private:
+    static const uint64_t symbol = N(EOS);
+    bool isnewuser( account_name user ) {
+      hacktable obj(_self, _self);
+      // get object by secondary key
+      auto hacks = obj.get_index<N(getbyuser)>();
+      auto hack = hacks.find(user);
+
+      return hack == hacks.end();
+    }
+     bool userexists( account_name user ) {
+      hacktable obj(_self, _self);
+      // get object by secordary key
+      auto hacks = obj.get_index<N(getbyuser)>();
+      auto hack = hacks.find(user);
+
+      return hack == hacks.end();
+    }
+
+
+    /// @abi table
+    struct hackstruct {
+      uint64_t      prim_key;  // primary key
+      account_name  user;      // account name for the user
+      std::string   hashdata;      // the hashed data
+      uint64_t      timestamp; // the store the last update block time
+
+      // primary key
+      auto primary_key() const { return prim_key; }
+      // secondary key: user
+      account_name get_by_user() const { return user; }
+    };
+
+    // create a multi-index table and support secondary key
+    typedef eosio::multi_index< N(hackstruct), hackstruct,
+      indexed_by< N(getbyuser), const_mem_fun<hackstruct, account_name, &hackstruct::get_by_user> >
+      > hacktable;
+
+     
+/// @abi action 
+    void get(const account_name _user) {
+        hacktable obj(_self, _self);
+        auto notes = obj.get_index<N(getbyuser)>();
+        auto iterator = notes.find(_user);
+        eosio_assert(iterator != notes.end(), "Account not found");
+        auto currentData = notes.get(_user);
+    print("Key: ", currentData.prim_key);
+    }
+    /// @abi action
+    /*void send_receipt(account_name buyer, std::string message){
+      action(
+        permission_level{get_self(), N(active)},
+        get_self(),
+        N(receipt),
+        std::make_tuple(buyer, message)
+      ).send();
+    }*/
+
+  public:
+    using contract::contract;
+
+     
+
+    /// @abi action
+    void add(const account_name _user, std::string& _hashdata) {
+        
+        //usernames are actually numbers under the account name
+        //require_auth(_user);
+       hacktable obj(_self, _self);
+        auto notes = obj.get_index<N(getbyuser)>();
+        auto iterator = notes.find(_user);
+        //eosio_assert(iterator != notes.end(), "User not found");
+        
+        
+
+ obj.emplace( _self, [&]( auto& address ) {
+          address.prim_key    = obj.available_primary_key();
+          address.user        = _user;
+          address.hashdata        = _hashdata;
+          address.timestamp   = now();
+        });
+
+       
+    }
+
+    /// @abi action
+
+     /*void deposit(account_name buyer, account_name seller, eosio::asset quantity, std::string memo) {
+
+      
+
+      balance_table balance(_self, buyer);
+      auto hodl_it = balance.find(symbol);
+
+      asset new_balance;
+      if(hodl_it != balance.end())
+        balance.modify(hodl_it, buyer, [&](auto& row){
+            row.funds -= quantity;
+            new_balance = row.funds;
+        });
+      else
+        balance.emplace(buyer, [&](auto& row){
+            row.funds = quantity;
+            new_balance  = row.funds;
+        });
+
+      // std::string message = name{buyer}.to_string() + " deposited " + quantity.amount + ", now has a total of " + new_balance;
+      send_receipt(buyer, "You deposited some EOS.");
+    }
+*/
+
+    /// @abi action
+    void buydata(account_name _buyer, account_name _seller, uint64_t price) 
+    {
+        hacktable obj(_self, _self);
+        auto notes = obj.get_index<N(getbyuser)>();
+        /*auto iterator_buyer = notes.find(_buyer);
+        eosio_assert(iterator_buyer != notes.end(), "Buyer not found");*/
+        auto currentData = notes.get(_seller);
+        //print("Key: ", currentData.prim_key);
+       
+       
+            auto iterator_seller = notes.find(_seller);
+            eosio_assert(iterator_seller != notes.end(), "Seller not found");
+                asset dataPrice = asset(price, string_to_symbol(4, "EOS"));
+                print("Price: ", dataPrice);
+                action(permission_level{_buyer,N(active)}, N(eosio), N(transfer), make_tuple(_buyer, _seller, dataPrice, std::string(""))).send();
+           
+    }
+    
+
+};
+
+// specify the contract name, and export a public action: update
+EOSIO_ABI( hacksf, (add) (buydata))
---
+++ eosio_docker/scripts/continue_blockchain.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# this file is used to continue the stopped blockchain
+
+echo "=== start blockchain with  ==="
+
+# set PATH
+PATH="$PATH:/opt/eosio/bin"
+
+set -m
+
+# start nodeos ( local node of blockchain )
+# run it in a background job such that docker run could continue
+nodeos -e -p eosio -d /mnt/dev/data \
+  --config-dir /mnt/dev/config \
+  --hard-replay \
+  --http-validate-host=false \
+  --plugin eosio::producer_plugin \
+  --plugin eosio::history_plugin \
+  --plugin eosio::chain_api_plugin \
+  --plugin eosio::history_api_plugin \
+  --plugin eosio::http_plugin \
+  --http-server-address=0.0.0.0:8888 \
+  --access-control-allow-origin=* \
+  --contracts-console \
+  --verbose-http-errors
+
+# `--hard-replay` option is needed
+# because the docker stop signal is not being passed to nodeos process directly
+# as we run the init_blockchain.sh as PID 1.
+
+sleep 2s
+echo "=== setup wallet: eosiomain ==="
+# First key import is for eosio system account
+cleos wallet create -n eosiomain --to-console | tail -1 | sed -e 's/^"//' -e 's/"$//' > eosiomain_wallet_password.txt
+cleos wallet import -n eosiomain --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+
+echo "=== setup wallet: hacksfwal ==="
+# key for eosio account and export the generated password to a file for unlocking wallet later
+cleos wallet create -n hacksfwal --to-console | tail -1 | sed -e 's/^"//' -e 's/"$//' > hacksf_wallet_password.txt
+# Owner key for hacksfwal wallet
+cleos wallet import -n hacksfawal --private-key 5JpWT4ehouB2FF9aCfdfnZ5AwbQbTtHBAwebRXt94FmjyhXwL4K
+# Active key for hacksfwal wallet
+cleos wallet import -n hacksfwal --private-key 5JD9AGTuTeD5BXZwGQ5AtwBqHK21aHmYnTetHgk1B3pjj7krT8N
